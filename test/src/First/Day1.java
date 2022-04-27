271c2d16e0fe12c9f9f4f52d52b3abf8d574b467
Saurab <sauravsilwal45@gmail.com>
2022-04-27 17:47:15 +0545

    further testing

--- test/src/First/Day1.java
+++ test/src/First/Day1.java
@@ -13,15 +13,30 @@ public class Day1 {
 		
 	}
 	 public void Login() {
-		 driver.findElement(By.name("uid")).sendKeys("gdchdcbj");
-		 driver.findElement(By.name("password")).sendKeys("12345568");
+		 driver.findElement(By.name("uid")).sendKeys("mngr398426");
+		 driver.findElement(By.name("password")).sendKeys("tymyqUs");
 		driver.findElement(By.name("btnLogin")).click();
 		 
+	}
+	 public void addCustomer() {
+		 driver.findElement(By.xpath("//a[contains(text(),'New Customer')]")).click();
+		 driver.findElement(By.name("name")).sendKeys("Ram");
+		 driver.findElement(By.xpath("//tbody/tr[5]/td[2]/input[1]")).click();
+		 driver.findElement(By.name("dob")).sendKeys("03/24/2000");
+		 driver.findElement(By.name("addr")).sendKeys("Kathmandu");
+		 driver.findElement(By.name("city")).sendKeys("");
+		 driver.findElement(By.name("state")).sendKeys("Bagamti");
+		 driver.findElement(By.name("pinno")).sendKeys("1234");
+		 driver.findElement(By.name("telephoneno")).sendKeys("9841383990");
+		 driver.findElement(By.name("emailid")).sendKeys("hari123@gmail.com");
+		 driver.findElement(By.name("password")).sendKeys("123jgdckjdsxj4");
+		 driver.findElement(By.name("submit")).click();		
 	}
 	public static void main(String[] args) {
 		Day1 d = new Day1();
 		 d.callBrowser();
 		 d.Login();
+		 d.addCustomer();
 	}
 	 
 }
