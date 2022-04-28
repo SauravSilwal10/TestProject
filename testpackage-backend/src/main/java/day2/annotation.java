8bb1550218f96d9fc2bb05dd870a75153c992d3d
Saurab <sauravsilwal45@gmail.com>
2022-04-28 15:35:13 +0545

    Annotation Flow

--- testpackage-backend/src/main/java/day2/annotation.java
+++ testpackage-backend/src/main/java/day2/annotation.java
@@ -13,40 +13,53 @@ import org.testng.annotations.AfterSuite;
 	public class annotation{
 	
 	  @Test
-	  public void f() {
-		  
+	  public void SearchCustomer() {
+		  System.out.println("Search For customer");
+	  }
+	  @Test
+	  public void SearchProduct() {
+		  System.out.println("Product for customer");
 	  }
 	  @BeforeMethod
 	  public void beforeMethod() {
+		  System.out.println("CSign In ");
 	  }
 
 	  @AfterMethod
 	  public void afterMethod() {
+		  System.out.println("Sign Out");
 	  }
 
 	  @BeforeClass
 	  public void beforeClass() {
+		  System.out.println("Open Test Application");
 	  }
+	  
+	  
 
 	  @AfterClass
 	  public void afterClass() {
+		  System.out.println("Close Test Application");
 	  }
 
 	  @BeforeTest
 	  public void beforeTest() {
+		  System.out.println("Open chrome");
 	  }
 
 	  @AfterTest
 	  public void afterTest() {
+		  System.out.println("Close Chrome");
 	  }
 
 	  @BeforeSuite
 	  public void beforeSuite() {
+		  System.out.println("chrome Set up property");
 	  }
 
 	  @AfterSuite
 	  public void afterSuite() {
-		  
+		  System.out.println("Clean up all cookies");
 	  }
 
 	}
