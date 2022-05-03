7ae2e83f8ca80059585c14db36a038da94f7647b
Saurab <sauravsilwal45@gmail.com>
2022-05-03 13:10:04 +0545

    Parameters

--- testpackage-backend/src/test/java/day2/orangehrm.java
+++ testpackage-backend/src/test/java/day2/orangehrm.java
@@ -1,24 +1,28 @@
 package day2;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 public class orangehrm extends driver{
+	@Parameters("url")
 	@Test
-	 public void callBrowser() {
+	 public void callBrowser(String url) {
 		 System.setProperty("webdriver.chrome.driver", "/home/ssilwal/Documents/selenium/chromedriver_linux64/chromedriver");
 		 driver = new ChromeDriver();
 		 driver.get(url);
 }
+	@Parameters({"userName", "password"})
 	@Test
-	public void login() {
-		 driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-		 driver.findElement(By.id("txtPassword")).sendKeys("admin123"); 	 
+	public void login( String userName ,String password ) {
+		 driver.findElement(By.id("txtUsername")).sendKeys(userName);
+		 driver.findElement(By.id("txtPassword")).sendKeys(password); 	 
 		 driver.findElement(By.id("btnLogin")).click();
 	}
 	@Test
-	 public void addCustomer() {
+	 public void info() {
 		 driver.findElement(By.id("menu_pim_viewMyDetails")).click();
 	}
 	
