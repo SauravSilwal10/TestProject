8c7416a13eb51533b0dd3bd610d898d09324e6c3
Saurab <sauravsilwal45@gmail.com>
2022-05-04 15:44:50 +0545

    More changes

--- testpackage-backend/src/test/java/day2/DependencyGroup.java
+++ testpackage-backend/src/test/java/day2/DependencyGroup.java
@@ -1,5 +1,38 @@
 package day2;
 
+import org.testng.annotations.Test;
+
 public class DependencyGroup {
+	@Test(groups ="Smoke")
+	public void test1() {
+		System.out.println("Test1 is runnning successfully ");
+	}
+	@Test(groups ="Smoke")
+	public void test2() {
+		System.out.println("Test2 is runnning successfully ");
+	
+	}
+	@Test(groups ="Smoke")
+	public void test3() {
+		System.out.println("Test2 is runnning successfully ");
+		
+}
+	@Test(groups ="Sanity")
+	public void test4() {
+		System.out.println("Test2 is runnning successfully ");
+		
+}
+	@Test(groups ="Regression")
+	public void test5() {
+		System.out.println("Test2 is runnning successfully ");
+		
+}
+	@Test( dependsOnGroups ={"Smoke", "Sanity","Regression"})
+	public void test0() {
+		System.out.println("Main test Case ");
+		
+	}
+
+
 
 }
