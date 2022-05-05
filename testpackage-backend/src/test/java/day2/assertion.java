6d75c54cf04e3d421cb5312fa2fccd70495111a9
Saurab <sauravsilwal45@gmail.com>
2022-05-05 18:33:29 +0545

    Changes

--- testpackage-backend/src/test/java/day2/assertion.java
+++ testpackage-backend/src/test/java/day2/assertion.java
@@ -7,23 +7,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class assertion extends driver {
-//	 @Test 
-//	 public void callBrowser() {
-//		 String url ="https://www.google.com/";
-//		 System.setProperty("webdriver.chrome.driver", "/home/ssilwal/Documents/selenium/chromedriver_linux64/chromedriver");
-//		 driver = new ChromeDriver();
-//		 driver.get(url);
-//}
-//	 @Test 
-//	public void gettitle() {
-//		driver.findElement(By.xpath("//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]")).sendKeys("Young Innovation");
-//		driver.findElement(By.xpath("//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[3]/center[1]/input[1]")).click();
-//		String expectedTitle = "Young Innovation - Google खोजी";
-//        String originalTitle = driver.getTitle();
-//        Assert.assertEquals(originalTitle,expectedTitle,"Title is matched");
-//        driver.quit();
-//	}
-//	 @Test
+	 @Test 
+	 public void callBrowser() {
+		 System.setProperty("webdriver.chrome.driver", "/home/ssilwal/Documents/selenium/chromedriver_linux64/chromedriver");
+		 driver = new ChromeDriver();
+}
+	 @Test 
+	public void gettitle() {
+		driver.findElement(By.xpath("//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]")).sendKeys("Young Innovation");
+		driver.findElement(By.xpath("//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[3]/center[1]/input[1]")).click();
+		String expectedTitle = "Young Innovation - Google खोजी";
+        String originalTitle = driver.getTitle();
+        Assert.assertEquals(originalTitle,expectedTitle,"Title is matched");
+        driver.quit();
+	}
+	 @Test
 	 public void facebook() {
 		 String url ="https://www.facebook.com/";
 		 System.setProperty("webdriver.chrome.driver", "/home/ssilwal/Documents/selenium/chromedriver_linux64/chromedriver");
@@ -33,24 +31,22 @@ public class assertion extends driver {
 	
 	 @Test
 	 public void title() {
-		 	String expectedTitle = "FacebookConnect with friends and the world around you on Facebook.";
+		 	String expectedTitle = "Facebook - log in or sign up";
 	        String originalTitle = driver.getTitle();
 	        Assert.assertEquals(originalTitle,expectedTitle,"Title is matched");
-	        driver.quit();
 	 }
 	 @Test
-	 public void Url() {
+	 public void url() {
 		 	
 		 	String expectedUrl= "https://www.facebook.com/";
 	        String originalUrl = driver.getCurrentUrl();
-	        Assert.assertEquals(originalUrl,expectedUrl,"Title is matched");
-	        driver.quit();
+	        Assert.assertEquals(originalUrl,expectedUrl,"Url  is matched");
 	 }
 	 @Test
-	 public void Text() {
+	 public void text() {
 		 	String expectedText= "";
 	        String originalText = driver.findElement(By.id("email")).getAttribute("value");
-	        Assert.assertEquals(originalText,expectedText,"Title is matched");
-	        driver.quit();
+	        Assert.assertEquals(originalText,expectedText,"Text is matched");
+//	        driver.quit();
 	 }
 }
